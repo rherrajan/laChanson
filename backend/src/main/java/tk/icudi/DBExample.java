14f6901986826303a0a3b8293aa6c207cc6b8ea4
rherrajan <currant@gmx.de>
2019-04-07 10:40:06 +0200

    .

--- backend/src/main/java/tk/icudi/DBExample.java
+++ backend/src/main/java/tk/icudi/DBExample.java
@@ -2,10 +2,19 @@ package tk.icudi;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
@@ -18,13 +27,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DBExample {
 
-	@Value("classpath:properties/systeminfo.json")
-	private Resource systeminfo;
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Autowired
+	private DataSource dataSource;
 	
 	@RequestMapping(value="/dbexample", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	String getSysteminfo() throws IOException {
-		return IOUtils.toString(systeminfo.getInputStream(), StandardCharsets.UTF_8); 
+	Object getDbexample() throws IOException {
+
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+			List<Timestamp> calls = new ArrayList<Timestamp>();
+			while (rs.next()) {
+				calls.add(rs.getTimestamp("tick"));
+			}
+
+			return calls;
+		} catch (Exception e) {
+			return "error: " + e;
+		}
 	}
 
 	@ModelAttribute
